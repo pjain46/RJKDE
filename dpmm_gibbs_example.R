397dcf92f4d7d0cfc6a141108b9de44122fc5735
pjain46 <46038600+pjain46@users.noreply.github.com>
2025-04-03 18:54:48 -0400

    cluster parameters

--- dpmm_gibbs_example.R
+++ dpmm_gibbs_example.R
@@ -24,16 +24,22 @@ alpha <- 1
 
 # Hyper parameters
 tau <- 1 # Known Precision for Normal - Normal conjugacy model
+
 mu0 <- 0 # Prior mean for cluster means
 tau0 <- 0.1 # Precision of prior for means
-
+a0 <- 2 # Shape for precision prior
+b0 <- 1 # Scale for precision prior
 
 
 # All data points in one cluster -- Initialize cluster assignment
 z = rep(1, n)
 k = 1 # Number of clusters
 
-for (iter in n_iter){
+# Store results
+cluster_history <- matrix(0, n_iter, n)
+n_clusters <- numeric(n_iter)
+
+for (iter in 1:n_iter){
 
   # Step 1: Update cluster assignments -- Chinese restaurant process
 
@@ -64,15 +70,15 @@ for (iter in n_iter){
 
       # Compute probability for cluster j: Likelihood × Cluster size
       # Posterior Predictive Distribution from conjugate model
-      dnorm(data[i], mu_j, sqrt(1/prec_j + tau)) * nj
+      dnorm(data[i], mu_j, sqrt(1/prec_j + 1/tau)) * nj
     })
 
-    new_cluster_prob <- alpha * dnorm(data[i], mu0, sqrt(1/tau0 + tau))
+    new_cluster_prob <- alpha * dnorm(data[i], mu0, sqrt(1/tau0 + 1/tau))
 
 
     # Sample new assignment for the data point
-    probs = c(cluster_probs, new_cluster_prob)
-    z[i] <- sample(1:(k+1), 1, probs)
+    probs <- c(cluster_probs, new_cluster_prob)
+    z[i] <- sample(1:(k+1), 1, prob = probs)
 
     # If we sampled the new cluster
     if (z[i] == k+1){
@@ -87,9 +93,26 @@ for (iter in n_iter){
   for (j in 1:k){
     nj <- sum(z == j)
     x_bar <- mean(data[z == j])
+
+    # Posterior parameters
+
+    # Posterior precision = Prior precision + Data precision
+    prec_j <- tau0 + nj*tau
+    mu_j <- (tau0*mu0 + tau*nj*x_bar)/prec_j
+
+    a_j <- a0 + nj/2
+    # b_new = b_0 + 0.5 * SSE within cluster + 0.5 * Penalty from deviating from mu_0
+    b_j <- b0 + 0.5 * sum((data[z == j] - x_bar)^2) +
+      (tau0 * nj * (x_bar - mu0)^2) / (2 * (tau0 + nj))
+
+    # Sample new parameters
+    cluster_precisions[j] <- rgamma(1, a_j, b_j)
+    cluster_means[j] <- rnorm(1, mu_j, sqrt(1/(prec_j * cluster_precisions[j])))
   }
 
-
+  # Store results
+  cluster_history[iter, ] <- z
+  n_clusters[iter] <- k
 
 }
 
